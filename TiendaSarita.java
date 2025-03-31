cdb84e0ff2575a5e3c53044e051f73c7af449784
CarlosOrozco <carlosorozcok@gmail.com>
2025-03-31 15:24:32 -0600

    Comentario de segundo commit linea 89

--- TiendaSarita.java
+++ TiendaSarita.java
@@ -86,6 +86,7 @@ public class TiendaSarita {
             System.out.println("No se realizo ningún tipo de descuento");
             System.out.println("Le invitamos a hacerse miembro para obtener beneficios");
             System.out.println("El monto minimo de compra para descuentos es de: Q. " + mDescuento);
+            System.out.println("Prueba de commit #2");
         }
        
     }
